51fd638b46ebbaaddf1c191792d810691cf2ea00
daihukumoshi <daihukumoshi@yahoo.co.jp>
2023-10-01 20:07:12 +0900

    feat:BookedUsersのバックエンド処理

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/user/:id', to: 'user#show', as: 'user'
+
+  get '/booked_users/:calendar_id', to: 'booked_users#show'
+  post '/booked_users/:calendar_id/:nickname/:password', to: 'booked_users#authenticate'
+
 end
