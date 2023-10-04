7fe385c1ec6952978f81484edae8ba173fd0434b
Hiroto2002 <hiroto2002h14@icloud.com>
2023-10-04 22:02:30 +0900

    feat:modal

---
+++ front/app/src/components/common/Modal.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import styles from "@/styles/Modal.module.css";
+import Image from "next/image";
+
+type Props = {
+  isOpen: boolean;
+  handleClose: () => void;
+  children: React.ReactNode;
+};
+export const Modal = (props: Props) => {
+  const { isOpen, children, handleClose } = props;
+
+  const closeWithClickOutSideMethod = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    setter: { (): void; (arg0: boolean): void }
+  ) => {
+    if (e.target === e.currentTarget) {
+      //メニューの外側をクリックしたときだけメニューを閉じる
+      console.log("メニューの外側をクリックした");
+      setter(false);
+    } else {
+      console.log("メニューの内側をクリックした");
+    }
+  };
+  return (
+    <>
+      {isOpen && (
+        <div
+          className={styles.overlay}
+          onClick={(e) => {
+            closeWithClickOutSideMethod(e, handleClose);
+          }}
+        >
+          <div className={styles.modalBody} onClick={() => {}}>
+            <div className={styles.buttonContainer}>
+              <button className={styles.closeButton} onClick={handleClose}>
+                <Image
+                  src={"/image/close.svg"}
+                  width={50}
+                  height={50}
+                  alt="close"
+                />
+              </button>
+            </div>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
