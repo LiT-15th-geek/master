ea9c71ec5db57971c751de79c13cd7dc33dfb1d9
Hiroto2002 <hiroto2002h14@icloud.com>
2023-10-16 22:10:02 +0900

    fix:readable

--- front/app/src/hooks/useCustomRouter.ts
+++ front/app/src/hooks/useCustomRouter.ts
@@ -2,10 +2,13 @@ import { useRouter } from "next/router";
 
 export const useCustomRouter = () => {
   const router = useRouter();
+  // ?userId=?的なのを取得する
   const getQueryId = router.query.id;
+  // page遷移
   const routerPush = (path: string) => {
     router.push(path);
   };
+  // 戻る
   const routerBack = () => {
     router.back();
   };
--- front/app/src/pages/index.tsx
+++ front/app/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useFirebase } from "@/hooks/useFirebase";
 import { GoogleAuthProvider } from "firebase/auth";
 import Image from "next/image";
 import styles from "@/styles/SignUp.module.css";
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useCustomRouter } from "@/hooks/useCustomRouter";
 
@@ -15,28 +15,39 @@ const Login = () => {
   const googleProvider = new GoogleAuthProvider();
   const { routerPush, getQueryId } = useCustomRouter();
   const [error, setError] = useState("");
+
+  // useForm = フォームのバリデーションを簡単にしてくれる
+  // register = バリデーションのルールを設定する
+  // handleSubmit = バリデーションを通った後に実行される処理
+  // formState = フォームの状態を取得できる
+  // errors = エラーの内容を取得できる
+  // type=submitとonSubmitが結びついてる
+  //
   const {
     handleSubmit,
     register,
     formState: { errors },
   } = useForm<Inputs>();
 
+  // googleログイン
   const handleGoogleLogin = async () => {
     const result = await SNSSignIn(googleProvider);
+    console.log(result);
+
     if (result === "success") {
       setError("");
       if (getQueryId) {
         routerPush(`/calendar/${getQueryId}`);
         return;
       }
-      routerPush("/");
+      routerPush("/profile");
       return;
     }
   };
 
+  // ログインボタンを押した時
   const onSubmit = async (data: Inputs) => {
     const result = await emailSignIn(data.email, data.password);
-    console.log(result);
 
     if (result === "success") {
       setError("");
@@ -52,6 +63,7 @@ const Login = () => {
     }
   };
 
+  // useEffect = []の中身に変更があった時に実行される(triggerみたいなもの)[空だとページの読み込んだ時、1回だけ処理が実行される]
   useEffect(() => {
     const loginCheck = async () => {
       const result = await getCurrentUser();
@@ -66,15 +78,18 @@ const Login = () => {
     };
     loginCheck();
   }, []);
+
   return (
     <>
       <>
         <div className={styles.container} style={{ height: "100vh" }}>
           <Image src="/image/edit.svg" width={70} height={70} alt="edit_icon" />
           <p className={styles.main_text}>情報を入力してください</p>
+          {/* onSubmitはボタンが押された時に走る処理 (type=submitとonSubmitが結びついてる[↓の場合]) */}
           <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
             <label>
               <p>メールアドレスを入力してください</p>
+              {/* ...registerがemailって名前をつけてる */}
               <input type="text" {...register("email", { required: true })} />
             </label>
             {errors.email && <span>This field is required</span>}
@@ -104,7 +119,6 @@ const Login = () => {
               新規登録
             </button>
           </div>
-         
         </div>
       </>
     </>
